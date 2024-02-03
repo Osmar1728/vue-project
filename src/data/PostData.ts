d86841b85c4997b2b9a7add65f76fd503b282871
JaimeCsDz <jaimecastaneda133@gmail.com>
2024-02-03 13:25:11 -0500

    Primer commit Emits

---
+++ src/data/PostData.ts
@@ -0,0 +1,33 @@
+import type { IPost } from "@/interfaces/IPost"
+const PostData:IPost[] =[
+    {
+        id: 1,
+        title : 'adjkshajds',
+        body : 'adjhdahduahsudhqasd',
+        date: new Date()
+    },
+    {
+        id: 2,
+        title : 'zxcvbnm,',
+        body : 'asdfghujikop',
+        date: new Date()
+    },
+    {
+        id: 3,
+        title : 'awzexbhnjmkl',
+        body : 'xesrdcfvgbhnj kmm',
+        date: new Date()
+    },
+    {
+        id: 4,
+        title : 'asdfghjukiyuhgdfrwefcsd ',
+        body : 'qwertyuiop',
+        date: new Date()
+    },
+    {
+        id: 5,
+        title : 'axcsdvnhmjy,kulgfytdrew',
+        body : 'qwzexrdctfvgybhnjmklxsrdbhnj',
+        date: new Date()
+    }
+];
